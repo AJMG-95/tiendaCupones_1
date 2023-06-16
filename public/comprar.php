fca6858b7e3e76351d4613d23506d6995a300641
amargue <antoniojesus.marchena@iesdonana.org>
2023-06-16 13:47:19 +0200

    primer commit

---
+++ public/comprar.php
@@ -0,0 +1,167 @@
+<?php session_start() ?>
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="/css/output.css" rel="stylesheet">
+    <title>Comprar</title>
+</head>
+
+<body>
+    <?php require '../vendor/autoload.php';
+
+    if (!\App\Tablas\Usuario::esta_logueado()) {
+        return redirigir_login();
+    }
+
+    $cuponCodigo = obtener_get('cupon');
+    $cupon = false;
+    $carrito = unserialize(carrito());
+
+    if (isset($cuponCodigo) && $cuponCodigo != '') {
+        $pdo = conectar();
+        $sentCupon = $pdo->prepare("SELECT * FROM cupones WHERE lower(unaccent(codigo)) LIKE lower(unaccent(:codigo))");
+        $sentCupon->execute([':codigo' => $cuponCodigo]);
+        $cupon = $sentCupon ? $sentCupon->fetch() : $cupon;
+
+        if ($cupon) {
+            if ($cupon['caducidad'] < date('Y-m-d')) {
+                $cupon = false;
+                $_SESSION['error'] = 'El cupón ha expirado';
+            }
+        } else {
+            $_SESSION['error'] = 'El cupón no existe';
+        }
+    }
+    
+    if (obtener_post('_testigo') !== null) {
+        $pdo = conectar();
+        $ids_art_carrito = implode(', ', $carrito->getIds());
+        $where = "WHERE id IN (" . $ids_art_carrito . ")";
+        $sent = $pdo->prepare("SELECT *
+                                FROM articulos
+                                $where");
+        $sent->execute();
+        $res = $sent->fetchAll(PDO::FETCH_ASSOC);
+
+        foreach ($res as $fila) {
+            if ($fila['stock'] < $carrito->getLinea($fila['id'])->getCantidad()) {
+                $_SESSION['error'] = 'No hay existencias suficientes para crear la factura.';
+                return volver();
+            }
+        }
+        // Crear factura
+        $usuario = \App\Tablas\Usuario::logueado();
+        $usuario_id = $usuario->id;
+        $pdo->beginTransaction();
+        $sent = $pdo->prepare('INSERT INTO facturas (usuario_id)
+                               VALUES (:usuario_id)
+                               RETURNING id');
+        $sent->execute([':usuario_id' => $usuario_id]);
+        $factura_id = $sent->fetchColumn();
+        $lineas = $carrito->getLineas();
+        $values = [];
+        $execute = [':f' => $factura_id];
+        $i = 1;
+
+        foreach ($lineas as $id => $linea) {
+            $values[] = "(:a$i, :f, :c$i)";
+            $execute[":a$i"] = $id;
+            $execute[":c$i"] = $linea->getCantidad();
+            $i++;
+        }
+
+        $values = implode(', ', $values);
+        $sent = $pdo->prepare("INSERT INTO articulos_facturas (articulo_id, factura_id, cantidad)
+                               VALUES $values");
+        $sent->execute($execute);
+        foreach ($lineas as $id => $linea) {
+            $cantidad = $linea->getCantidad();
+            $sent = $pdo->prepare('UPDATE articulos
+                                      SET stock = stock - :cantidad
+                                    WHERE id = :id');
+            $sent->execute([':id' => $id, ':cantidad' => $cantidad]);
+        }
+
+        if ($cupon) {
+            $cuponId = $cupon['id'];
+            $sentFactura = $pdo->prepare("UPDATE facturas SET cupon_id = :cuponId WHERE id = :id");
+            $sentFactura->execute([":id" => $factura_id, ":cuponId" => $cuponId]);
+        }
+
+        $pdo->commit();
+        $_SESSION['exito'] = 'La factura se ha creado correctamente.';
+        unset($_SESSION['carrito']);
+        return volver();
+    }
+
+    ?>
+
+    <div class="container mx-auto">
+        <?php require '../src/_menu.php' ?>
+        <?php require '../src/_alerts.php' ?>
+        <div class="overflow-y-auto py-4 px-3 bg-gray-50 rounded dark:bg-gray-800">
+            <form action="" method="get">
+                <label for="cupon">Cupon de descuento: </label>
+                <input type="text" name="cupon" id="cupon" class="rounded-lg" value="<?= isset($cupon) ? $cupon['codigo'] : '' ?>">
+                <button type="submit" href="" class="mx-auto focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-900">Aplicar</button>
+            </form>
+            <table class="mx-auto text-sm text-left text-gray-500 dark:text-gray-400">
+                <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                    <th scope="col" class="py-3 px-6">Código</th>
+                    <th scope="col" class="py-3 px-6">Descripción</th>
+                    <th scope="col" class="py-3 px-6">Cantidad</th>
+                    <th scope="col" class="py-3 px-6">Precio</th>
+                    <th scope="col" class="py-3 px-6">Importe</th>
+                </thead>
+                <tbody>
+                    <?php $total = 0 ?>
+                    <?php foreach ($carrito->getLineas() as $id => $linea) : ?>
+                        <?php
+                        $articulo = $linea->getArticulo();
+                        $codigo = $articulo->getCodigo();
+                        $cantidad = $linea->getCantidad();
+                        $precio = $articulo->getPrecio();
+                        $importe = $cantidad * $precio;
+                        $total += $importe;
+                        ?>
+                        <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                            <td class="py-4 px-6"><?= $articulo->getCodigo() ?></td>
+                            <td class="py-4 px-6"><?= $articulo->getDescripcion() ?></td>
+                            <td class="py-4 px-6 text-center"><?= $cantidad ?></td>
+                            <td class="py-4 px-6 text-center">
+                                <?= dinero($precio) ?>
+                            </td>
+                            <td class="py-4 px-6 text-center">
+                                <?= dinero($importe) ?>
+                            </td>
+                        </tr>
+                    <?php endforeach;
+
+                    if (isset($cupon)) {
+                        $total = $total - $total * $cupon['descuento'];
+                    } else {
+                    }
+
+                    ?>
+
+                </tbody>
+                <tfoot>
+                    <td colspan="3"></td>
+                    <td class="text-center font-semibold">TOTAL:</td>
+                    <td class="text-center font-semibold"><?= dinero($total) ?></td>
+                </tfoot>
+            </table>
+            <form action="" method="POST" class="mx-auto flex mt-4">
+                <input type="hidden" name="_testigo" value="1">
+                <button type="submit" href="" class="mx-auto focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-900">Realizar pedido</button>
+            </form>
+        </div>
+    </div>
+    <script src="/js/flowbite/flowbite.js"></script>
+</body>
+
+</html>
---
+++ src/tienda.sql
@@ -0,0 +1,67 @@
+DROP TABLE IF EXISTS articulos CASCADE;
+
+CREATE TABLE articulos (
+    id          bigserial     PRIMARY KEY,
+    codigo      varchar(13)   NOT NULL UNIQUE,
+    descripcion varchar(255)  NOT NULL,
+    precio      numeric(7, 2) NOT NULL,
+    stock       int           NOT NULL
+);
+
+DROP TABLE IF EXISTS usuarios CASCADE;
+
+CREATE TABLE usuarios (
+    id       bigserial    PRIMARY KEY,
+    usuario  varchar(255) NOT NULL UNIQUE,
+    password varchar(255) NOT NULL,
+    validado bool         NOT NULL
+);
+
+DROP TABLE IF EXISTS facturas CASCADE;
+
+CREATE TABLE facturas (
+    id         bigserial  PRIMARY KEY,
+    created_at timestamp  NOT NULL DEFAULT localtimestamp(0),
+    usuario_id bigint NOT NULL REFERENCES usuarios (id),
+    cupon_id   bigint REFERENCES usuarios (id)
+);
+
+DROP TABLE IF EXISTS articulos_facturas CASCADE;
+
+CREATE TABLE articulos_facturas (
+    articulo_id bigint NOT NULL REFERENCES articulos (id),
+    factura_id  bigint NOT NULL REFERENCES facturas (id),
+    cantidad    int    NOT NULL,
+    PRIMARY KEY (articulo_id, factura_id)
+);
+
+DROP TABLE IF EXISTS cupones CASCADE;
+
+CREATE TABLE cupones (
+    id bigserial PRIMARY KEY,
+    codigo varchar(255) UNIQUE NOT NULL,
+    descuento double precision NOT NULL CHECK (descuento >= 0 AND descuento <= 1),
+    caducidad date NOT NULL,
+    cupon varchar (255) UNIQUE NOT NULL 
+);
+
+-- Carga inicial de datos de prueba:
+
+INSERT INTO articulos (codigo, descripcion, precio, stock)
+    VALUES ('18273892389', 'Yogur piña', 200.50, 4),
+           ('83745828273', 'Tigretón', 50.10, 2),
+           ('51736128495', 'Disco duro SSD 500 GB', 150.30, 0),
+           ('83746828273', 'Tigretón', 50.10, 3),
+           ('51786128435', 'Disco duro SSD 500 GB', 150.30, 5),
+           ('83745228673', 'Tigretón', 50.10, 8),
+           ('51786198495', 'Disco duro SSD 500 GB', 150.30, 1);
+
+INSERT INTO usuarios (usuario, password, validado)
+    VALUES ('admin', crypt('admin', gen_salt('bf', 10)), true),
+           ('pepe', crypt('pepe', gen_salt('bf', 10)), false);
+
+INSERT INTO cupones (codigo, descuento, caducidad, cupon)
+VALUES ('CUPON20', 0.20, '2023-12-31', 'CUPON20DESC'),
+       ('CUPON50', 0.50, '2023-12-31', 'CUPON50DESC'),
+       ('CUPON70', 0.70, '2023-1-31', 'CUPON70DESC');
+
